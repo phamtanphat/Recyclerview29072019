469ff1ea3e9fabc660218a28c35bc324471205f0
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-16 18:18:30 +0700

    A

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/recyclerview29072019/MonanAdapter.java
@@ -0,0 +1,54 @@
+package khoapham.ptp.phamtanphat.recyclerview29072019;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MonanAdapter extends RecyclerView.Adapter<MonanAdapter.MonanHolder> {
+
+    ArrayList<Monan> mangmonan;
+
+    public MonanAdapter(ArrayList<Monan> mangmonan) {
+        this.mangmonan = mangmonan;
+    }
+
+    @NonNull
+    @Override
+    public MonanHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_monan,null);
+        return new MonanHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MonanHolder holder, int position) {
+        holder.
+    }
+
+    @Override
+    public int getItemCount() {
+        return mangmonan.size() >0 ? mangmonan.size() : 0;
+    }
+
+
+    class MonanHolder extends RecyclerView.ViewHolder{
+        TextView txtTen,txtGia;
+        ImageView imgHinhanh;
+        public MonanHolder(@NonNull View itemView) {
+            super(itemView);
+            if (itemView == null){
+                txtGia = itemView.findViewById(R.id.textviewGia);
+                txtTen = itemView.findViewById(R.id.textviewTen);
+                imgHinhanh = itemView.findViewById(R.id.imageItemMonan);
+            }
+
+        }
+    }
+}
